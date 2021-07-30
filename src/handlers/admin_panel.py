be841f861b98aee0919ca5f052f03c81bd6124a8
top1Dron <pro100moneyfarmer@gmail.com>
2021-07-30 13:14:33 +0300

    fix stop

--- src/handlers/admin_panel.py
+++ src/handlers/admin_panel.py
@@ -47,26 +47,26 @@ async def stop_command(message: types.Message):
         db_utils.delete_group(chat_id=message.chat.id)
 
 
-@dp.message_handler(lambda message: message.from_user.id in ADMINS and 
+@dp.message_handler(lambda message:
     message.chat.id in db_utils.get_bot_groups(), commands=['rm'])
 async def process_rm_keyboard(message):
     await message.reply('remove keyboard', reply_markup=types.ReplyKeyboardRemove())
 
 
-@dp.message_handler(lambda message: message.from_user.id in ADMINS and
+@dp.message_handler(lambda message:
     message.text == 'Управление словами' and message.chat.id in db_utils.get_bot_groups())
 async def get_forbidden_words(message: types.Message):
     forbidden_words = db_utils.get_forbidden_words()
     await message.reply(f'Запрещённые слова: {forbidden_words}', reply_markup=get_manage_words_kb())
 
 
-@dp.message_handler(lambda message: message.from_user.id in ADMINS and 
+@dp.message_handler(lambda message:
     message.text == 'Удаление по ключевым словам' and message.chat.id in db_utils.get_bot_groups())
 async def delete_messages_with_forbidden_words_command(message: types.Message):
     await misc.delete_old_messages_with_forbidden_words()
 
 
-@dp.callback_query_handler(lambda callback_query: callback_query.from_user.id in ADMINS, 
+@dp.callback_query_handler(lambda callback_query: True, 
     words_cb.filter(action=['back']), state=[WordsStates.ADD_WORD, WordsStates.REMOVE_WORD])
 async def process_go_back(callback_query: types.CallbackQuery, state: FSMContext):
     '''
@@ -84,7 +84,7 @@ async def process_go_back(callback_query: types.CallbackQuery, state: FSMContext
         reply_markup=get_manage_words_kb())
 
 
-@dp.callback_query_handler(lambda callback_query: callback_query.from_user.id in ADMINS and
+@dp.callback_query_handler(lambda callback_query:
     callback_query.message.chat.id in db_utils.get_bot_groups(), 
     words_cb.filter(action=['add']), state='*')
 async def process_add_forbidden_word(callback_query: types.CallbackQuery):
@@ -99,7 +99,7 @@ async def process_add_forbidden_word(callback_query: types.CallbackQuery):
         reply_markup=get_back_menu())
 
 
-@dp.message_handler(lambda message: message.from_user.id in ADMINS and 
+@dp.message_handler(lambda message:
     message.chat.id in db_utils.get_bot_groups(), state=WordsStates.ADD_WORD)
 async def enter_new_word(message: types.Message, state: FSMContext):
     '''
@@ -127,7 +127,7 @@ async def enter_new_word(message: types.Message, state: FSMContext):
     await state.finish()
 
 
-@dp.callback_query_handler(lambda callback_query: callback_query.from_user.id in ADMINS and
+@dp.callback_query_handler(lambda callback_query:
     callback_query.message.chat.id in db_utils.get_bot_groups(), 
     words_cb.filter(action=['remove']), state='*')
 async def process_remove_forbidden_word(callback_query: types.CallbackQuery):
@@ -142,8 +142,8 @@ async def process_remove_forbidden_word(callback_query: types.CallbackQuery):
         reply_markup=get_back_menu())
 
 
-@dp.message_handler(lambda message: message.from_user.id in ADMINS 
-    and message.chat.id in db_utils.get_bot_groups(), state=WordsStates.REMOVE_WORD)
+@dp.message_handler(lambda message:  
+    message.chat.id in db_utils.get_bot_groups(), state=WordsStates.REMOVE_WORD)
 async def delete_word(message: types.Message, state: FSMContext):
     '''
     remove forbidden word from database
@@ -163,7 +163,7 @@ async def delete_word(message: types.Message, state: FSMContext):
     await state.finish()
 
 
-@dp.message_handler(lambda message: message.from_user.id in ADMINS and 
+@dp.message_handler(lambda message: 
     message.text == 'Удаление сообщений пользователя' and 
     message.chat.id in db_utils.get_bot_groups(), state=None)
 async def enter_user(message: types.Message):
