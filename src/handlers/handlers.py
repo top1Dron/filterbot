18225c74b4c47561142cfdc1413a7bc443b0c8ac
top1Dron <pro100moneyfarmer@gmail.com>
2021-08-01 13:22:57 +0300

    Fix help command and add strip filter to all words

--- src/handlers/handlers.py
+++ src/handlers/handlers.py
@@ -6,6 +6,13 @@ from loader import dp
 from utils import db_utils, misc
 
 
+@dp.message_handler(lambda message: message.chat.id in db_utils.get_bot_groups(), commands=['help'])
+async def help_command(message: types.Message):
+    help_message = 'Я бот-антиспам, фильтрую спам и ненормативную лексику.' \
+        'Надеюсь на ваше понимание, уважайте друг друга.'
+    await message.reply(help_message)
+
+
 @dp.message_handler(lambda message: message.chat.id in db_utils.get_bot_groups(), state=None)
 @dp.throttled(misc.anti_flood, rate=1/1.5)
 async def filter_messages(message: types.Message):
@@ -20,22 +27,9 @@ async def filter_messages(message: types.Message):
     )
     forbidden_words = db_utils.get_forbidden_words()
     for word in forbidden_words:
-        if word.lower() in message.text.lower().split():
+        message_text_list = [text.strip(' ,\'"\t\n<>') for text in message.text.lower().split()]
+        if word.lower() in message_text_list:
             await message.delete()
             db_utils.delete_message(message_id=message.message_id, chat_id=message.chat.id)
             await message.answer('Сообщение было удалено из-за наличия ненормативной лексики!')
             break
-
-
-@dp.message_handler(lambda message: message.chat.id in db_utils.get_bot_groups(), commands=['help'])
-async def help_command(message: types.Message):
-    help_message = 'Я бот-антиспам, фильтрую спам и ненормативную лексику.' \
-        'Надеюсь на ваше понимание, уважайте друг друга.'
-    if message.from_user.id in ADMINS:
-        help_message += '\nУ меня есть следующие команды:\n' \
-            '/rm - удалить все текстовые кнопки\n' \
-            '/forbidden_words - просмотр/добавление/удаление ключевых слов,\n' \
-            'по которым фильтруются сообщения\n' \
-            '/cancel - отменяет действие при вводе на удаление/добавление слова в базу данных'
-    await message.reply(help_message)
-    await misc.delete_old_messages_with_forbidden_words()
